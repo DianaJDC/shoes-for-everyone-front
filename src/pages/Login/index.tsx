a479cf437cc22800274df7a1f4b5a4eb3ea3216d
Diana Mora <dianacmorag9937@gmail.com>
2023-04-24 18:19:38 -0300

    validaçao feita

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,21 +1,125 @@
 import Button from "../../components/Button";
 import LabelInput from "../../components/LabelInput";
-import "../Login/login.css";
+import React, { useState } from "react";
+
+interface LoginState {
+  email: string;
+  password: string;
+  emailError: string;
+  passwordError: string;
+  isEmailOk: boolean;
+  isPasswordOk: boolean;
+}
+
+const initialState: LoginState = {
+  email: "",
+  password: "",
+  emailError: "",
+  passwordError: "",
+  isEmailOk: false,
+  isPasswordOk: false,
+};
 
 const Login = () => {
+  const [formState, setFormState] = useState<LoginState>(initialState);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /.{8,}/g;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+    if (name === "email") {
+      if (emailRegex.test(value)) {
+        setFormState((prev) => ({
+          ...prev,
+          emailError: "",
+          isEmailOk: true,
+        }));
+      } else {
+        setFormState((prev) => ({
+          ...prev,
+          emailError: "Por favor, introduce una dirección de correo electrónico válida.",
+          isEmailOk: false,
+        }));
+      }
+    }
+
+    if (name === "password") {
+      if (passwordRegex.test(value)) {
+        setFormState((prev) => ({
+          ...prev,
+          passwordError: "",
+          isPasswordOk: true,
+        }));
+      } else {
+        setFormState((prev) => ({
+          ...prev,
+          passwordError: "La contraseña debe tener al menos 8 caracteres.",
+          isPasswordOk: false,
+        }));
+      }
+    }
+
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  
+    if (!formState.isEmailOk) {
+      setFormState((prev) => ({
+        ...prev,
+        emailError: "Por favor, introduce una dirección de correo electrónico válida.",
+      }));
+    }
+  
+    if (!formState.isPasswordOk) {
+      setFormState((prev) => ({
+        ...prev,
+        passwordError: "La contraseña debe tener al menos 8 caracteres.",
+      }));
+    }
+  };
+
   return (
-    <div className="rectangledm">
-      <div>
-        <div className="emaildm">
-          <LabelInput labelText="Email"></LabelInput>
-        </div>
-        <div>
-          <LabelInput labelText="Senha"></LabelInput>
-        </div>
-        <div>
-          <Button>Entrar</Button>
-        </div>
-      </div>
+    <div className="flex justify-center items-center h-[calc(100vh-80px)]">
+      <form
+        onSubmit={handleSubmit}
+        className="p-10 bg-white rounded shadow-md flex flex-col gap-10 w-[560px]"
+      >
+        <LabelInput
+          type="email"
+          labelText="Email"
+          name="email"
+          placeholder="Email"
+          value={formState.email}
+          onChange={handleInputChange}
+          required
+        ></LabelInput>
+        {formState.emailError && <div className="text-red-500">{formState.emailError}</div>}
+
+        <LabelInput
+          type="password"
+          name="password"
+          labelText="Senha"
+          placeholder="Senha"
+          value={formState.password}
+          onChange={handleInputChange}
+          required
+        ></LabelInput>
+        {formState.passwordError && <div className="text-red-500">{formState.passwordError}</div>}
+
+        <Button
+          type="submit"
+          className="self-start"
+          disabled={!(formState.isEmailOk && formState.isPasswordOk)}
+        >
+          Entrar
+        </Button>
+      </form>
     </div>
   );
 };
